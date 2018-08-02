f95ca508164463e7824f1f62e6507185d27e22bb
Zully Barrientos <zullybarrientos@example.com>
2018-08-02 13:21:59 -0700

    5cp5-assignment

--- app.js
+++ app.js
@@ -1,46 +1,52 @@
-
 function onReady() {
       const addToDoForm = document.getElementById('addToDoForm');
      const newToDoText = document.getElementById('newToDoText');
      const toDoList = document.getElementById('toDoList');
 
-     addToDoForm.addEventListener('submit', () => {
-       event.preventDefault();
 
-       // get the text
-    let title = newToDoText.value;
+  addToDoForm.addEventListener('submit', () => {
+  event.preventDefault();
 
-    // create a new li
-    let newLi = document.createElement('li');
+  // get the text
+let title = newToDoText.value;
 
-    // create a new input
-    let checkbox = document.createElement('input');
+// create a new li
+let newLi = document.createElement('li');
 
-    // set the input's type to checkbox
-    checkbox.type = "checkbox";
+// create a new input
+let checkbox = document.createElement('input');
 
-    // set the title
-    newLi.textContent = title;
+// set the input's type to checkbox
+checkbox.type = "checkbox";
 
-    // attach the checkbox to the li
-    newLi.appendChild(checkbox);
+// set the title
+newLi.textContent = title;
 
-    // attach the li to the ul
-        toDoList.appendChild(newLi);
+// attach the checkbox to the li
+newLi.appendChild(checkbox);
 
-    //empty the input
-    newToDoText.value = '';
+// attach the li to the ul
+   toDoList.appendChild(newLi);
 
+//empty the input
+newToDoText.value = '';
 
-  // Delete Button
-  deleteButton.addEventListener('markCheckbox'() => {
-    event.preventDefault();
-    //delete item
-      toDoList.removeChild(newLi);
-    }
-  });
+});
+
+  const deleteButton = document.getElementByID('deleteButton');
+
+  deleteButton.addEventListener('delete', () => {
+  event.preventDefault();
+
+  // create a new li
+  let newLi = document.createElement('li');
+
+  //delete the li from the ul
+      newLi.toDoList.removeChild(newLi);
+});
+
 }
-  
+
 window.onload = function() {
 onReady();
- };
+};
