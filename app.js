a472a4b3135f197b8d4a7e4e987a4135ee4a577b
Zully Barrientos <zullybarrientos@example.com>
2018-08-02 19:14:46 -0700

    5CP5-assignment

--- app.js
+++ app.js
@@ -7,44 +7,44 @@ function onReady() {
   addToDoForm.addEventListener('submit', () => {
   event.preventDefault();
 
-  // get the text
-let title = newToDoText.value;
+    // get the text
+    let title = newToDoText.value;
 
-// create a new li
-let newLi = document.createElement('li');
+    // create a new li
+    let newLi = document.createElement('li');
 
-// create a new input
-let checkbox = document.createElement('input');
+    // create a new input
+    let checkbox = document.createElement('input');
 
-// set the input's type to checkbox
-checkbox.type = "checkbox";
+    // set the input's type to checkbox
+    checkbox.type = "checkbox";
 
-// set the title
-newLi.textContent = title;
+    // set the title
+    newLi.textContent = title;
 
-// attach the checkbox to the li
-newLi.appendChild(checkbox);
+    // attach the checkbox to the li
+    newLi.appendChild(checkbox);
 
-// attach the li to the ul
-   toDoList.appendChild(newLi);
+    // attach the li to the ul
+    toDoList.appendChild(newLi);
 
-//empty the input
-newToDoText.value = '';
+    //empty the input
+    newToDoText.value = '';
+
+    /*attach delete button to the li */
+    let button = document.createElement('button');
+    button.innerText = 'Delete';
+    newLi.appendChild(button);
+
+          button.addEventListener('click', () => {
+		      event.preventDefault();
+
+          //delete the li from the ul
+		       toDoList.removeChild(newLi);
+         });
+
 
 });
-
-  const deleteButton = document.getElementByID('deleteButton');
-
-  deleteButton.addEventListener('delete', () => {
-  event.preventDefault();
-
-  // create a new li
-  let newLi = document.createElement('li');
-
-  //delete the li from the ul
-      newLi.toDoList.removeChild(newLi);
-});
-
 }
 
 window.onload = function() {
